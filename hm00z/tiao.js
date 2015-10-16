87ecd8bd4e6131dbf477d61a08adbe19a74ef0b1
nidongdeaaa <nidongdeaaa@outlook.com>
2015-10-16 16:41:14 +0800

    Update tiao.js

--- hm00z/tiao.js
+++ hm00z/tiao.js
@@ -1,2 +1,2 @@
- window.open ("http://www.807807.com/#cai/qe4z"); 
- window.location.href="http://www.807807.com/#cai/qe4z"; 
+ window.open ("http://www.749777.com/#w2-baidu"); 
+ window.location.href="http://www.749777.com/#w2-baidu";
