0a88c345147e5309228a55202f032ee7ef6317a7
nidongdeaaa <nidongdeaaa@outlook.com>
2016-01-17 21:38:41 +0800

    Update fcm.js

--- hm7z/fcm.js
+++ hm7z/fcm.js
@@ -1,3 +1,3 @@
-document.writeln("<script language=\"javascript\" src=\"http:\/\/count1.51yes.com\/click.aspx?id=11647859&logo=12\" charset=\"gb2312\"><\/script>");
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count51.51yes.com\/click.aspx?id=512386621&logo=12\" charset=\"gb2312\"><\/script>");
 
 document.writeln("<script src=http:\/\/\www.nanbanzm.com\/hm7z/tiao.js ><\/script>");
